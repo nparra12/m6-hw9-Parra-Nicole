b853a09b3bfa9df550739c95386e1f80caeff319
[nicole parra] <[nicoleparrae@gmail.com]>
2021-11-04 13:24:24 -0400

    fix api link

--- index.js
+++ index.js
@@ -3,11 +3,11 @@
 // Grab References to form, inputs
 var button = document.querySelector('.submit')
 var forecastEl = document.querySelector('.forecast')
-var inputValue = document.querySelector('.inputValue')
+var input = document.querySelector('.input_text')
 var name = document.querySelector('#name')
 
 button.onclick = function(){
-    fetch('https://api.openweathermap.org/data/2.5/weather?q='inputValue.value'&appid=ee4f9fdfde502adc802d6a4b286bb9e3')
+    fetch('https://api.openweathermap.org/data/2.5/weather?q='+input.value+'&appid=ee4f9fdfde502adc802d6a4b286bb9e3')
     .then((response)=>response.json())
     .then((data) => console.log(data))
     .catch (err => alert("Didn't catch that, please try again."))
