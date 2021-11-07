207ff7fd83a91b81f0b41928f9d3f5cc1f8da189
[nicole parra] <[nicoleparrae@gmail.com]>
2021-11-07 10:32:59 -0500

    add styling

--- index.js
+++ index.js
@@ -1,17 +1,15 @@
 //API key: ee4f9fdfde502adc802d6a4b286bb9e3
 
 // Grab References to form, inputs
-var button = document.querySelector('.submit')
-var forecastEl = document.querySelector('.forecast')
-var input = document.querySelector('.input_text')
-var name = document.querySelector('#name')
+var button = document.querySelector('submit')
+var forecastEl = document.querySelector('forecast')
+var inputValue = document.querySelector('input_text')
+var name = document.querySelector('name')
 
-button.onclick = function(){
-    fetch('https://api.openweathermap.org/data/2.5/weather?q='+input.value+'&appid=ee4f9fdfde502adc802d6a4b286bb9e3')
+button.addEventListener('click', function(){
+    fetch('https://api.openweathermap.org/data/2.5/weather?q='+inputValue+'&appid=ee4f9fdfde502adc802d6a4b286bb9e3')
     .then((response)=>response.json())
     .then((data) => console.log(data))
-    .catch (err => alert("Didn't catch that, please try again."))
 
-}
-
-//'https://api.openweathermap.org/data/2.5/weather?q='+inputValue+'&aee4f9fdfde502adc802d6a4b286bb9e3'
+    .catch(err => alert("Try again!"))
+})
