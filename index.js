f25e9a66daa9210b058e03232a54799454b16c8a
[nicole parra] <[nicoleparrae@gmail.com]>
2021-11-07 19:46:24 -0500

    add values

--- index.js
+++ index.js
@@ -1,15 +1,38 @@
 //API key: ee4f9fdfde502adc802d6a4b286bb9e3
 
 // Grab References to form, inputs
-var button = document.querySelector('submit')
-var forecastEl = document.querySelector('forecast')
-var inputValue = document.querySelector('input_text')
-var name = document.querySelector('name')
+var button = document.querySelector('.submit')
+var forecastEl = document.querySelector('.forecast')
+var inputValue = document.querySelector('.input_text')
+var cityName = document.querySelector('.cityName')
+var desc = document.querySelector('.desc')
+var feelsLike = document.querySelector('.feelsLike')
 
 button.addEventListener('click', function(){
-    fetch('https://api.openweathermap.org/data/2.5/weather?q='+inputValue+'&appid=ee4f9fdfde502adc802d6a4b286bb9e3')
+    fetch('https://api.openweathermap.org/data/2.5/weather?q='+inputValue.value+'&appid=ee4f9fdfde502adc802d6a4b286bb9e3')
     .then((response)=>response.json())
-    .then((data) => console.log(data))
+    .then((data)=> {
+        var nameValue = data['name']
+        var descValue = data['weather'][0]['description']
+        var temperatureValue = data['main']['temp']
+        var feelslikesValue = data['main']['feels_like']
+        
 
-    .catch(err => alert("Try again!"))
+        cityName.innerHTML = nameValue
+        console.log(cityName, nameValue)
+
+        desc.innerHTML = descValue
+        console.log(desc)
+
+        forecastEl.innerHTML = temperatureValue
+        console.log(forecastEl)
+
+        feelsLike.innerHTML = feelslikesValue
+
+      
+    
+    })
+  
+
+    
 })
