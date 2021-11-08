080fc73b957ae6aba9a79fcdddaf6d15c24f47f6
[nicole parra] <[nicoleparrae@gmail.com]>
2021-11-08 17:46:39 -0500

    show symbols

--- index.js
+++ index.js
@@ -7,19 +7,26 @@ var inputValue = document.querySelector('.input_text')
 var cityName = document.querySelector('.cityName')
 var desc = document.querySelector('.desc')
 var feelsLike = document.querySelector('.feelsLike')
+var icon = document.querySelector('#icon')
 
 button.addEventListener('click', function(){
     fetch('https://api.openweathermap.org/data/2.5/weather?q='+inputValue.value+'&units=imperial&appid=ee4f9fdfde502adc802d6a4b286bb9e3')
     .then((response)=>response.json())
     .then((data)=> {
         var nameValue = data['name']
+
+        var iconValue = data['weather'][0]['icon']
+        var iconUrl = "http://openweathermap.org/img/w/" + iconValue + ".png"
+    
         var descValue = data['weather'][0]['description']
         var temperatureValue = data['main']['temp']
         var feelslikesValue = data['main']['feels_like']
-        
 
         cityName.innerHTML = nameValue
         console.log(cityName, nameValue)
+
+        icon.innerHTML = ("<img src='" + iconUrl  + "'>")
+        console.log(inputValue)
 
         desc.innerHTML = descValue
         console.log(desc)
